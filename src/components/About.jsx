afd97da15a9b0eaa008dc5259fdb41074ce0456f
septanto <s.s.adi.nugroho@gmail.com>
2024-09-05 22:38:23 +0700

    initial

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about" className="container mx-auto p-6">
+      <h2 className="text-3xl font-bold mb-4">About Me</h2>
+      <p className="text-gray-700">
+        I am an experienced software developer with a passion for building scalable and efficient applications...
+      </p>
+    </section>
+  );
+}
+
+export default About;
