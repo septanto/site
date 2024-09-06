7a99aa396292b432cd7bdbf313d652a011334e27
Siprianus Septanto Adi Nugroho <s.s.adi.nugroho@gmail.com>
2024-09-06 21:29:36 +0700

    Move build to vite (#1)
    * remove app css
    
    * move build to vite
    
    * add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import About from "./components/About"
+import Services from "./components/Services"
+import Contact from "./components/Contact"
+
+function App() {
+  return (
+      <div>
+        <Header />
+        <main>
+          <About />
+          <Services />
+          <Contact />
+        </main>
+      </div>
+  )
+}
+
+export default App
